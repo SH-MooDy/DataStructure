bfc51b3480f432aa8984872db8ab1b2c292ed71f
SH-MooDy <shlee4167@naver.com>
2024-09-30 14:20:19 +0900

    2주차 과제 수정

--- week2/prob1.cpp
+++ week2/prob1.cpp
@@ -1,32 +1,37 @@
-#include<iostream>
-#include<string>
-#include<vector>
-#include<algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 int main() {
     vector<string> strs;
     string word;
+
     while (1) {
         cin >> word;
-        if (word == "exit") { break; }
-
-        // 벡터에 중복된 단어를 가리키는 이터레이터
-        auto it = find(strs.begin(), strs.end(), word);
-
-        // 중복이 있으면 중복 단어 제거, 없으면 벡터에 단어 추가 후 정렬
-        if (it != strs.end()) {
-            strs.erase(it);
+        if (word == "exit") {
+            break;
         }
+
+        // 벡터를 정방향으로 탐색
+        auto it = lower_bound(strs.begin(), strs.end(), word);
+
+        // 중복된 단어가 있으면 삭제
+        if (it != strs.end() && *it == word) {
+            strs.erase(it);  // 단어 삭제
+        } 
         else {
-            strs.push_back(word);
-            sort(strs.begin(), strs.end());
+            // 중복이 없으면 해당 위치에 단어 삽입
+            strs.insert(it, word);
         }
 
-        for (auto s : strs) {
+        // 벡터의 내용 출력
+        for (const auto &s : strs) {
             cout << s << " ";
         }
         cout << endl;
     }
+
     return 0;
 }
