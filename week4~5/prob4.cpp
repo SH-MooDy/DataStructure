35c33b2486d2c492edb0855f07a848cbfe39ef73
SH-MooDy <shlee4167@naver.com>
2024-10-10 13:18:54 +0900

    수정

--- week4~5/prob4.cpp
+++ week4~5/prob4.cpp
@@ -30,41 +30,39 @@ int main() {
 }
 
 void ordered_insert(string item) {
-    /* head와 tail이 가리키는 2중 연결리스트에 문자열들이 사전식 순서로 정렬된 순서를 유지하 도록 새로운 문자열 item을 삽입한다. */
-    Node *head = nullptr;
-    Node *tail = nullptr;
-    Node *p = nullptr;
+    // head와 tail이 가리키는 2중 연결리스트에 문자열들이 사전식 순서로 정렬된 순서를 유지하도록 새로운 문자열 item을 삽입한다.
+    Node *p = head;
 
     Node *new_node = new Node;
     new_node->data = item;
     new_node->prev = nullptr;
     new_node->next = nullptr;
 
-    if(p == nullptr && head == nullptr) {
+    if (head == nullptr) { // 리스트가 비어있을 때
         head = new_node;
         tail = new_node;
     }
     else {
-        while(p != nullptr && item < p->data) {
-            if(p->data > item) {
-                if(p == head) { // 가장 앞에 삽입하는 경우우
-                    new_node->next = head;
-                    head->prev = new_node;
-                    head = new_node;
-                }
-                else if(p == tail) { // 가장 뒤에 삽입하는 경우
-                    new_node->prev = tail;
-                    tail->next = new_node;
-                    tail = new_node;
-                }
-                else {
-                    new_node->prev = p;
-                    new_node->next = p->next;
-                    p->next->prev = new_node;
-                    p->next = new_node;
-                }
-            }
-            p = p->prev;
+        // 삽입 위치를 찾기 위해 순차적으로 탐색
+        while (p != nullptr && p->data < item) {
+            p = p->next;
+        }
+
+        if (p == nullptr) { // 삽입 위치가 리스트의 끝인 경우
+            new_node->prev = tail;
+            tail->next = new_node;
+            tail = new_node;
+        }
+        else if (p == head) { // 삽입 위치가 리스트의 맨 앞인 경우
+            new_node->next = head;
+            head->prev = new_node;
+            head = new_node;
+        }
+        else { // 중간에 삽입하는 경우
+            new_node->prev = p->prev;
+            new_node->next = p;
+            p->prev->next = new_node;
+            p->prev = new_node;
         }
     }
 }
