8e5a9135102ad1f6c19f9c7495aecc5626d90710
SH-MooDy <shlee4167@naver.com>
2024-10-14 17:30:05 +0900

    update prob1

--- week6~7/prob1.cpp
+++ week6~7/prob1.cpp
@@ -1,16 +1,18 @@
 #include<iostream>
 #include<vector>
-
+#include<list>
+#include<fstream>
 using namespace std;
 
 int id_counter = 0; // 각 Song 객체마다 서로 다른 index를 부여하기 위해서 전역변수 선언
 
+struct Artist; 
 struct Song {
     int index;
-    string title, album, my_url;
+    string title, album, mv_url;
     Artist *artist;
     Song() {}
-    Song(string ti, Artist *art, string alb, string mv): title(ti), artist(art), album(alb), my_url(mv) {
+    Song(string ti, Artist *art, string alb, string mv): title(ti), artist(art), album(alb), mv_url(mv) {
         index = id_counter++; // 생성된 모든 Song 객체에서 유일한 index를 부여함
     }
 };
@@ -22,6 +24,7 @@ struct Artist {
     Artist(string name): name(name) {}
 };
 
+
 list<Artist *> artist_directory[256]; // 이름의 첫 문자를 배열 인덱스로 사용
 
 const int SONG_DIRECTORY_SIZE = 10;
@@ -29,7 +32,7 @@ list<Song *> song_directory[SONG_DIRECTORY_SIZE];
 
 const string datafilename = "songs.csv";
 
-Artist *find_arist(string name) {
+Artist *find_artist(string name) {
     list<Artist *> artist_list = artist_directory[(unsigned char)name[0]];
     for (auto it = artist_list.begin(); it != artist_list.end(); it++) {
         if((*it)->name == name) {
@@ -49,7 +52,7 @@ void print_artist(Artist *p) {
 void print_artist_directory() {
     for(int i=0; i<256; i++) {
         list<Artist *> &artist_list = artist_directory[i];
-        for(quto ptr: artist_list) {
+        for(auto ptr: artist_list) {
             print_artist(ptr);
         }
     }
@@ -58,6 +61,7 @@ void print_artist_directory() {
 Artist *add_artist(string name) {
     Artist *ptr_artist = new Artist(name);
     list<Artist *> &artist_list = artist_directory[(unsigned char)name[0]];
+    artist_list.push_back(ptr_artist);
     return ptr_artist;
 }
 
@@ -72,7 +76,21 @@ void add_song(string title, string artist, string album = "", string mv_url = ""
     song_list.push_back(song_ptr);
 }
 
-void pritn_song_directory() {
+void load_songs(string filename) {
+    string line;
+    ifstream songfile(filename);
+    while(getline(songfile, line)) {
+        vector<string> tokens = spilt_line(line, ',');
+        assert(tokens.size()==4 || tokens.size()==3);
+        if(tokens.size()==4)
+            add_song(tokens[0], tokens[1], tokens[2], tokens[3]);
+        else
+            add_song(tokens[0], tokens[1], tokens[2]);
+    }
+    songfile.close();
+}
+
+void print_song_directory() {
     for(int i=0; i<SONG_DIRECTORY_SIZE; i++) {
         list<Song *> &song_list = song_directory[i];
         for(auto s: song_list) {
@@ -85,7 +103,7 @@ int main() {
     load_songs(datafilename);
 
     print_artist_directory();
-    pritn_song_directory();
-    
+    print_song_directory();
+
     return 0;
 }
