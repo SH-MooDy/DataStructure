c3db601a8f20582aab2acc1d0e99b52dea6bbcf4
SH-MooDy <shlee4167@naver.com>
2024-10-14 17:06:20 +0900

    add week6

--- week6~7/prob1.cpp
+++ week6~7/prob1.cpp
@@ -3,18 +3,89 @@
 
 using namespace std;
 
-int id_counter = 0;
+int id_counter = 0; // 각 Song 객체마다 서로 다른 index를 부여하기 위해서 전역변수 선언
 
-struct song {
+struct Song {
     int index;
     string title, album, my_url;
     Artist *artist;
     Song() {}
-    Song(string ti, Artist *art, string alb, string mv): title(ti), artist(art), album
-, my_url(mv) {
-    
-}};
-int main() {
+    Song(string ti, Artist *art, string alb, string mv): title(ti), artist(art), album(alb), my_url(mv) {
+        index = id_counter++; // 생성된 모든 Song 객체에서 유일한 index를 부여함
+    }
+};
 
+struct Artist {
+    string name;
+    list<Song *> songs;
+    Artist() {}
+    Artist(string name): name(name) {}
+};
+
+list<Artist *> artist_directory[256]; // 이름의 첫 문자를 배열 인덱스로 사용
+
+const int SONG_DIRECTORY_SIZE = 10;
+list<Song *> song_directory[SONG_DIRECTORY_SIZE];
+
+const string datafilename = "songs.csv";
+
+Artist *find_arist(string name) {
+    list<Artist *> artist_list = artist_directory[(unsigned char)name[0]];
+    for (auto it = artist_list.begin(); it != artist_list.end(); it++) {
+        if((*it)->name == name) {
+            return *it;
+        }
+    }
+    return nullptr;
+}
+
+void print_artist(Artist *p) {
+    cout << p->name << ":" << endl;
+    for(auto s: p->songs) {
+        cout << "    " << s->index << ":" << s->title << ", " << s->album << ", " << s->mv_url << endl;
+    }
+}
+
+void print_artist_directory() {
+    for(int i=0; i<256; i++) {
+        list<Artist *> &artist_list = artist_directory[i];
+        for(quto ptr: artist_list) {
+            print_artist(ptr);
+        }
+    }
+}
+
+Artist *add_artist(string name) {
+    Artist *ptr_artist = new Artist(name);
+    list<Artist *> &artist_list = artist_directory[(unsigned char)name[0]];
+    return ptr_artist;
+}
+
+void add_song(string title, string artist, string album = "", string mv_url = "") {
+    Artist *artist_ptr = find_artist(artist);
+    if (artist_ptr == nullptr) {
+        artist_ptr = add_artist(artist);
+    }
+    Song *song_ptr = new Song(title, artist_ptr, album, mv_url);
+    artist_ptr->songs.push_back(song_ptr);
+    list<Song *> &song_list = song_directory[song_ptr->index%SONG_DIRECTORY_SIZE];
+    song_list.push_back(song_ptr);
+}
+
+void pritn_song_directory() {
+    for(int i=0; i<SONG_DIRECTORY_SIZE; i++) {
+        list<Song *> &song_list = song_directory[i];
+        for(auto s: song_list) {
+            cout << "    " << s->index << ":" << s->title << ", " << s->artist->name << ", " << s->album << ", " << s->mv_url << endl;
+        }
+    }
+}
+
+int main() {
+    load_songs(datafilename);
+
+    print_artist_directory();
+    pritn_song_directory();
+    
     return 0;
 }
