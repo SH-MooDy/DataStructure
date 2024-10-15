3eea0a2aff622d8550ece0d68c4368794a2ca288
SH-MooDy <shlee4167@naver.com>
2024-10-15 17:00:07 +0900

    prob1 add

--- week6~7/prob1.cpp
+++ week6~7/prob1.cpp
@@ -2,12 +2,15 @@
 #include<vector>
 #include<list>
 #include<fstream>
+#include<cassert>
+#include<sstream>
+#include<string>
 using namespace std;
 
 int id_counter = 0; // 각 Song 객체마다 서로 다른 index를 부여하기 위해서 전역변수 선언
 
 struct Artist; 
-struct Song {
+struct Song { // 하나의 노래 정보를 저장하는 구조체  
     int index;
     string title, album, mv_url;
     Artist *artist;
@@ -17,7 +20,7 @@ struct Song {
     }
 };
 
-struct Artist {
+struct Artist { // 한명의 아티스트 정보를 저장하는 구조체
     string name;
     list<Song *> songs;
     Artist() {}
@@ -31,6 +34,28 @@ const int SONG_DIRECTORY_SIZE = 10;
 list<Song *> song_directory[SONG_DIRECTORY_SIZE];
 
 const string datafilename = "songs.csv";
+
+string trim(string str) {
+    int s = 0, t = str.length()-1;
+    while(s<str.length() && isspace(str[s]))
+        s++;
+    while(t>=0 && isspace(str[t]))
+        t--;
+
+    if(s<=t)
+        return str.substr(s,t-s+1);
+    else    
+        return "";
+}
+
+vector<string> spilt_line(string &line, char delimeter) {
+    vector<string> tokens;
+    stringstream sstream(line);
+    string str;
+    while(getline(sstream, str, delimeter))
+        tokens.push_back(trim(str));
+    return tokens;
+}
 
 Artist *find_artist(string name) {
     list<Artist *> artist_list = artist_directory[(unsigned char)name[0]];
@@ -81,7 +106,7 @@ void load_songs(string filename) {
     ifstream songfile(filename);
     while(getline(songfile, line)) {
         vector<string> tokens = spilt_line(line, ',');
-        assert(tokens.size()==4 || tokens.size()==3);
+        assert(tokens.size()==4 || tokens.size()==3);  
         if(tokens.size()==4)
             add_song(tokens[0], tokens[1], tokens[2], tokens[3]);
         else
@@ -102,8 +127,31 @@ void print_song_directory() {
 int main() {
     load_songs(datafilename);
 
-    print_artist_directory();
-    print_song_directory();
+    string command, arguments;
+    while(1) {
+        cout << "$ ";
+        cin >> command;
 
+        if (command == "exit") { break; }
+
+        if(command == "list") {
+        
+        }
+        else if(command == "add") {
+
+        }
+        else if(command == "find") {
+
+        }
+        else if(command == "play") {
+
+        }
+        else if(command == "remove") {
+
+        }
+        else if(command == "save") {
+
+        }
+    }
     return 0;
 }
