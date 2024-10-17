94fe5fc122f81af9dcdf559a9f0df6a2218312f2
SH-MooDy <shlee4167@naver.com>
2024-10-17 11:25:12 +0900

    공부하기 싫다

--- group_activity/test.cpp
+++ group_activity/test.cpp
@@ -4,7 +4,14 @@ void func(Node *first, int lower, int upper) {
     Node *p = first, Node *q = nullptr;
     while(p->next != nullptr) {
         if(p->data >= lower && p->data <= upper) {
-            
+            if(p == first) {
+                first = p->next;
+                delete p;
+                p = first;
+            }
+            else {
+                
+            }
         }
         q = p;
         p = p->next;
