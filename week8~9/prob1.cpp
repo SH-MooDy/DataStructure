8f1fbd7d5e6b28cca9e7cb74d2fb89377a64f4f8
SH-MooDy <shlee4167@naver.com>
2024-11-03 22:04:22 +0900

    prob1 complete

--- week8~9/prob1.cpp
+++ week8~9/prob1.cpp
@@ -2,7 +2,7 @@
 #include<string>
 using namespace std;
 
-const int MAX_CAPACITY;
+const int MAX_CAPACITY = 100;
 class Stack {
 private:
     int stack[MAX_CAPACITY];
@@ -35,15 +35,26 @@ public:
         if(empty()) {
             throw runtime_error("stacK_empty");
         }
+        return stack[top_pos];
     }
 };
 
 int main() {
+    Stack s1;
     string str;
     getline(cin, str);
 
+    int i = 1; // 괄호의 번호를 메기기 위한 인덱스
     for(auto c : str) {
-        
+        if(c == '(') {
+            s1.push(i);
+            cout << i << " ";
+            i++;
+        }
+        else if(c == ')') {
+            cout << s1.top() << " ";
+            s1.pop();
+        }
     }
 
     return 0;
