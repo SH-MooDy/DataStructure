0a8869597c57c0644d517161edd7b0901abe9f73
SH-MooDy <shlee4167@naver.com>
2024-11-07 11:35:53 +0900

    과제 추가

--- week8~9/prob3.cpp
+++ week8~9/prob3.cpp
@@ -1,12 +1,14 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
+
 const int MAX_CAPACITY = 1000000;
 
 class Stack {
 private:
-    int stack[MAX_CAPACITY];
+    char stack[MAX_CAPACITY];
     int top_pos = -1;
-    int count = 0;
 
 public:
     bool full() {
@@ -17,12 +19,11 @@ public:
         return top_pos == -1;
     }
 
-    void push(int i) {
-        if(full()) {
+    void push(char i) {
+        if (full()) {
             throw runtime_error("stack_full");
         }
         stack[++top_pos] = i;
-        count++;
     }
 
     void pop() {
@@ -30,20 +31,49 @@ public:
             throw runtime_error("stack_empty");
         }
         top_pos--;
-        count--;
     }
 
-    int top() {
-        if(empty()) {
-            throw runtime_error("stacK_empty");
+    char top() {
+        if (empty()) {
+            throw runtime_error("stack_empty");
         }
         return stack[top_pos];
     }
-    
+
     int size() {
-        return count;
+        return top_pos + 1;
+    }
+
+    string getStackContent() {
+        string result;
+        for (int i = 0; i <= top_pos; i++) {
+            result += stack[i];
+        }
+        return result;
     }
 };
+
 int main() {
+    Stack s1;
+    string num;
+    int remove_digit;
+
+    cin >> num;
+    cin >> remove_digit;
+
+    int digits_to_keep = num.size() - remove_digit;
+
+    for (auto n : num) {
+        while (!s1.empty() && s1.top() < n && remove_digit > 0) {
+            s1.pop();
+            remove_digit--;
+        }
+        s1.push(n);
+    }
+
+    // 스택의 상위 요소만 남겨놓기 위해 잘라냄
+    string result = s1.getStackContent().substr(0, digits_to_keep);
+    cout << result << endl;
+
     return 0;
 }
---
+++ week8~9/prob4.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<stack>
+#include<fstream>
+using namespace std;
+
+const int MAX_SIZE = 100;
+pair<int, int> position;
+
+
+
+int main() {
+    ifstream infile("input.txt");
+    int T, N;
+    infile >> T;
+    for(int i=0; i<T; i++) {
+        
+    }
+    return 0;
+}
