94ece89368314681a717033bf4bb6012b723021f
SH-MooDy <shlee4167@naver.com>
2024-11-07 23:00:52 +0900

    임시저장

--- week8~9/prob4.cpp
+++ week8~9/prob4.cpp
@@ -5,19 +5,18 @@ using namespace std;
 
 const int MAX_SIZE = 100;
 
-/*
-const int PATH = 0; // 지나갈 수 있는 위치
-const int WALL = 1; // 지나갈 수 없는 위치
-const int VISITED = 2; // 이미 방문한 위치
-const int BACKTRACKED = 3; // 방문했다가 되돌아 나온 위치
-*/
 const int BACKGROUND = 0; // 배경 픽셀
 const int IMAGE = 1; // 이미지 픽셀
-const int VISITED_BACK = 2; // 방문한 배경 픽셀
-const int VISITED_IMAGE = 3; // 방문한 이미지 픽셀
-const int BACKTRACKED_IMAGE; // 방문헀다가 되돌아 나온 이미지 픽셀
+const int VISITED_IMAGE = 2; // 방문한 이미지 픽셀
+const int BACKTRACKED_IMAGE = 3; // 방문헀다가 되돌아 나온 이미지 픽셀
 
 int input_IMG[MAX_SIZE][MAX_SIZE];
+
+struct Position {
+    int x, y;
+    Position() {}   
+    Position(int x, int y): x(x), y(y) {}
+};
 
 void read_images(int size, ifstream &infile) {    
     for(int i=0; i<size; i++) {
@@ -27,8 +26,23 @@ void read_images(int size, ifstream &infile) {
     }
 }
 
+int offset[4][2] = {{-1, 0}.
+                    {0, 1},
+                    {1, 0}.
+                    {0, -`1}};
+
+bool movable(Position pos, int dir, int n) {
+    int x = pos.x + offset[dir][0];
+    int y = pos.y + offset[dir][1];
+    return x>=0 && x<n && y>=0 && input_IMG[x][y]==0;
+}
+
+Position move_to(Position pos, int dir) {
+    return Position(pos.x + offset[dir][0], pos.y + offset[dir][1]);
+}
+
 int main() {
-    pair<int, int> position; // 좌표를 나타내는 pair
+    pair<int, int> position = 0,0; // 현재 좌표를 나타내는 pair
     ifstream infile("input.txt");
     int T, N;
     infile >> T;
@@ -38,10 +52,17 @@ int main() {
         stack<pair<int, int>> s1; // 좌표를 저장하는 스택
 
         while(true) {
-
+            
         }
     }
 
     infile.close();
     return 0;
 }
+
+/*
+    대충 계획
+    1. 배경부터 스캔
+    2. 만약 이미지 픽셀(1)을 찾으면 근처에 연결된 이미지 픽셀들을 서칭
+    3. 서칭하면서 
+*/
