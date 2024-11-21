03c95be3e340eb1171baa4610d55aab1a8925a01
SH-MooDy <shlee4167@naver.com>
2024-11-21 10:50:28 +0900

    group activity 6 update

---
+++ maze_prob/maze_by_cueue.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+  read_maze();
+  queue < pair<int, int> que;
+  pair<int, int> cur(0, 0);
+  maze[cur.first][cur.second] = -1;
+  que.push(cur);
+
+  while (!que.empty()) {
+    cur = que.
+  }
+  return 0;
+}
