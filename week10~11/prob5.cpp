4f95d8804ee8d84810ec2b474bdf444bef874b21
SH-MooDy <shlee4167@naver.com>
2024-11-23 23:22:37 +0900

    prob 5 완료

--- week10~11/prob5.cpp
+++ week10~11/prob5.cpp
@@ -1,66 +1,59 @@
 #include <fstream>
 #include <iostream>
 #include <queue>
-#include <vector>
 using namespace std;
 
-const int MAX_CAPACITY = 100;
-int MAZE[MAX_CAPACITY][MAX_CAPACITY];
-int N;
+const int MAX_SIZE = 100;
+int maze[MAX_SIZE][MAX_SIZE];
+int N, count_route = 0;
 
-struct Position {
-  pair<int, int> pos;
-  int dir;  // 이전 이동 방향을 저장
-  Position(int x, int y, int dir) : pos(x, y), dir(dir) {}
-};
+const int PATHWAY = 0;  // 지나갈 수 있는 길
+const int WALL = 1;     // 벽
+const int PATH = 2;     // 방문한 길
 
-void read_file() {
+int offset[4][2] = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+
+void read_maze() {
   ifstream infile("input2.txt");
   infile >> N;
 
   for (int i = 0; i < N; i++) {
     for (int j = 0; j < N; j++) {
-      infile >> MAZE[i][j];
+      infile >> maze[i][j];
     }
   }
 
   infile.close();
 }
 
-int offset[4][2] = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+void findMazePath(int x, int y) {
+  // 범위를 벗어나는 경우
+  if (x < 0 || y < 0 || x >= N || y >=N || maze[x][y] != PATHWAY) {
+    return;
+  }
 
-bool movable(pair<int, int> pos, int dir) {
-  int x = pos.first + offset[dir][0];
-  int y = pos.second + offset[dir][1];
-  return x >= 0 && x < N && y >= 0 && y < N && MAZE[x][y] == 0;
-}
+  // 도착한 경우
+  if (x == N - 1 && y == N - 1) {
+    count_route++;
+    return;
+  }
 
-pair<int, int> move_to(pair<int, int> pos, int dir) {
-  return {pos.first + offset[dir][0], pos.second + offset[dir][1]};
+  maze[x][y] = PATH;  // 현재 방문 위치
+
+  for (int i = 0; i < 4; i++) {
+    int nx = x + offset[i][0];
+    int ny = y + offset[i][1];
+
+    findMazePath(nx, ny);
+  }
+
+  // 경로 복원
+  maze[x][y] = PATHWAY;
 }
 
 int main() {
-  read_file();
-  queue<Position> que;
-  Position start(0, 0, -1);  // 첫 칸은 방향이 없으므로 -1로 초기화
-  que.push(start);
-
-  while (!que.empty()) {
-    Position cur = que.front();
-    que.pop();
-
-    // 도착지 도착 시
-    if (cur.pos.first == N - 1 && cur.pos.second == N - 1) {
-      cout << cur.curve_count << endl;
-      return 0;
-    }
-
-    for (int dir = 0; dir < 4; dir++) {
-      if (movable(cur.pos, dir)) {
-        pair<int, int> p = move_to(cur.pos, dir);
-      }
-    }
-  }
-  cout << "No path exists." << endl;
+  read_maze();
+  findMazePath(0, 0);
+  cout << count_route << endl;
   return 0;
 }
