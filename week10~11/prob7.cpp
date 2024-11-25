028cdc1d59dbe300f05579c276fc2ececfdc385d
SH-MooDy <shlee4167@naver.com>
2024-11-25 17:35:59 +0900

    prob7 update

--- week10~11/prob7.cpp
+++ week10~11/prob7.cpp
@@ -11,34 +11,44 @@ void read_file() {
   while (infile >> num) {
     lucky_numbers.push_back(num);
   }
+  infile.close();
 }
 
-bool isLuckyNum(int k, int count) {
-  for (int i = 0; i < lucky_numbers.size(); i++) {
-    if ((i + 1) % count == 0) {
-      if (lucky_numbers[i] == k) {
-        return false;
-      }
+bool isLuckyNum(int k, int step) {
+  // 종료 조건: 제거 기준(step)이 배열 크기 이상이면 종료
+  if (step > lucky_numbers.size()) {
+    return true;
+  }
 
-      lucky_numbers.erase(lucky_numbers.begin() + i);
+  // 현재 단계에서 제거 작업 수행
+  vector<int> filtered_numbers;
+  for (int i = 0; i < lucky_numbers.size(); i++) {
+    if ((i + 1) % step != 0) { // step 기준에 해당하지 않는 숫자만 남김
+      filtered_numbers.push_back(lucky_numbers[i]);
+    } else if (lucky_numbers[i] == k) {
+      // k가 제거 대상이라면 행운의 숫자가 아님
+      return false;
     }
   }
 
-  if (!isLuckyNum) {
-    return false;
-  }
-  return true;
+  // lucky_numbers 갱신
+  lucky_numbers = filtered_numbers;
+
+  // 다음 단계로 이동
+  return isLuckyNum(k, step + 1);
 }
 
 int main() {
   read_file();
+
   int k;
   cin >> k;
 
-  if (isLuckyNum(k, 0)) {
+  if (isLuckyNum(k, 2)) {  // step은 2부터 시작
     cout << "yes" << endl;
   } else {
     cout << "no" << endl;
   }
+
   return 0;
 }
