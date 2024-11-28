db0ba78fe2408aa1d712c1207fa12d9b3d1d7761
SH-MooDy <shlee4167@naver.com>
2024-11-28 13:55:05 +0900

    prob7 modify

--- week10~11/prob7.cpp
+++ week10~11/prob7.cpp
@@ -1,44 +1,39 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
 using namespace std;
 
-bool isLuckyNum(int n) {
-  vector<int> numbers;
-  for (int i = 1; i <= 1000000; i++) {
-    numbers.push_back(i);
+const int MAXNUM = 1000000;
+
+void filterNumbers(vector<int>& numbers, int step) {
+  if (step > numbers.size()) {
+    return;  // 더 이상 제거할 숫자가 없으면 종료
   }
 
-  int step = 2;
-  while (step <= numbers.size()) {
-    vector<int> filtered;
-    for (int i = 0; i < numbers.size(); i++) {
-      if ((i + 1) % step != 0) {
-        filtered.push_back(numbers[i]);
-      }
-    }
-    numbers = filtered;
-
-    if (n < numbers.size() && numbers[n - 1] == n) {
-      break;
-    }
-
-    step++;
-  }
-
-  // 행운수 여부 확인
-  for (int num : numbers) {
-    if (num == n) {
-      return true;
+  vector<int> filtered;
+  for (int i = 0; i < numbers.size(); i++) {
+    if ((i + 1) % step != 0) {
+      filtered.push_back(numbers[i]);
     }
   }
-  return false;
+
+  numbers = filtered;
+  filterNumbers(numbers, step + 1);
 }
 
 int main() {
-  int n;
-  cin >> n;
+  int k;
+  cin >> k;
 
-  if (isLuckyNum(n)) {
+  vector<int> numbers(MAXNUM);
+  for (int i = 1; i <= MAXNUM; i++) {
+    numbers[i - 1] = i;
+  }
+
+  filterNumbers(numbers, 2);  // 2단계부터 시작
+
+  // k가 남아 있는지 확인
+  if (find(numbers.begin(), numbers.end(), k) != numbers.end()) {
     cout << "yes" << endl;
   } else {
     cout << "no" << endl;
