15ab0b0afb786b6143f688756d0d5db3043885cd
SH-MooDy <shlee4167@naver.com>
2024-11-29 12:31:26 +0900

    prob 7 update

--- week10~11/prob8.cpp
+++ week10~11/prob8.cpp
@@ -5,6 +5,8 @@ using namespace std;
 const int MAX = 16;
 
 int board[MAX][MAX];
+pair<int, int> start;
+pair<int, int> dest;
 
 void read_file() {
   ifstream infile("input8.txt");
@@ -15,8 +17,27 @@ void read_file() {
       infile >> board[i][j];
     }
   }
+
+  infile >> start.first;
+  infile >> start.second;
+  infile >> dest.first;
+  infile >> dest.second;
 }
+
+bool isFindToken(int x, int y) {
+  for (int dir = 0; i < 4; i++) {
+  }
+}
+
+bool canMovePo() {}
+
 int main() {
   read_file();
+
+  if (canMovePo) {
+    cout << "Yes" << Endl;
+  } else {
+    cout << "No" << endl;
+  }
   return 0;
 }
